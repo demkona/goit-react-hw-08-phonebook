359f215ef83a84421f4340b05a56493322317829
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2022-11-06 16:36:38 +0200

    fix error delete manifest

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,7 +1,5 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
-console.log(`getIsLoggedIn`, getIsLoggedIn);
 const getUsername = state => state.auth.user.name;
-
 const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
 
 const authSelectors = {
