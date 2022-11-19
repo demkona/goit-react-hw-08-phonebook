9929bf14a29d75069f93eaf33bb02ca17863b910
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2022-11-19 21:47:03 +0200

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import News from 'components/News/News';
 import Weather from 'components/Weather/Weather';
 import { BsFillJournalBookmarkFill } from 'react-icons/bs';
+import css from '../pages/pages.module.css'
 
 export default function HomePage() {
 
 
   return (
     <>
-      <h1>
+      <h1 className={css.titleHome}>
         Hello, I'm your Phone Book{' '}
         <BsFillJournalBookmarkFill />
       </h1>
