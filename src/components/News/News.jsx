499475af3955949cb1e0ac3a1fb8f0d87eaebe92
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2022-11-19 15:46:35 +0200

    add component news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,11 +1,32 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getNews } from 'service/service';
+import css from '../News/News.module.css'
 
-export const News = ({ news }) => {
-  console.log(news)
+export const News = () => {
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    getNews().then(data => { setNews(data); }).catch(console.log);
+  }, []);
+
+  if (!news) {
+    return null;
+  }
+  const oneNews = news.articles
+  console.log(oneNews)
+
   return (
     <>
-      {/* <ul>
-        {news.map(({ news }) => (<li>{news}</li>))}
-      </ul> */}
+      <ul className={css.newItem} >
+        {oneNews.map(({ title, url, urlToImage }) => (<li key={title} >
+          <Link className={css.link} to={url} target="_blank">
+            {title}
+            <img src={urlToImage} alt={title} width='300' />
+          </Link>
+        </li>))}
+
+      </ul>
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,16 @@
 import News from 'components/News/News';
-import { useState, useEffect } from 'react';
 import { BsFillJournalBookmarkFill } from 'react-icons/bs';
-import { getNews } from 'service/service';
-
 
 export default function HomePage() {
-  const [news, setNews] = useState(null);
 
-  useEffect(() => {
-    getNews()
-      .then(data => {
-        setNews(data);
-      })
-  }, []);
 
-  console.log(news)
   return (
-    <div>
+    <>
       <h1>
         Hello, I'm your Phone Book{' '}
         <BsFillJournalBookmarkFill />
       </h1>
-      <News news={news} />
-    </div>
+      <News />
+    </>
   );
 };
